f0323f20757246413b14c2fcf5060aa191aa6bfa
pacocampo <paco.ocampor@gmail.com>
2016-06-29 06:47:55 -0500

    statics fixed

--- scrapper/settings.py
+++ scrapper/settings.py
@@ -107,7 +107,7 @@ USE_TZ = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 print(PROJECT_ROOT)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
